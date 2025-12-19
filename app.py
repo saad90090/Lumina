bebc9da3e5d2391228e09bc58ed87e38e288d6a1
Saad Ahmad <saad.ahmad90090@gmail.com>
2025-12-19 07:10:19 +0500

    Update main block to use environment variable for port

--- app.py
+++ app.py
@@ -182,5 +182,6 @@ def download_image():
     
     return send_file(save_path, as_attachment=True)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
